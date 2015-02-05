aef1971b7c29bb0c9724346b76cd9e92f8a57cc2
Levi Tan Ong <levi@byimplication.com>
2015-02-05 23:02:01 +0800

    adds about section

--- Torch.jsx
+++ Torch.jsx
@@ -11,15 +11,30 @@ var Torch = React.createClass({
     return (
       <div className="Torch">
         <Donate />
+
         <section id="banner">
           <i id="logo" className="icon-torch" />
-          <h1>
-            Torch Apps
-          </h1>
+          <h1>Torch Apps</h1>
           <p className="tagline">
             Mutating the state
           </p>
         </section>
+
+        <section id="about">
+          <div className="container">
+            <p>
+              Torch (formerly FroHawk) is an independent development team based in Manila. We advocate transparency, accountability, and citizen engagement. We make tools for these.
+            </p>
+
+            <p>
+              <a className="inline" target="_blank" href="https://docs.google.com/forms/d/1rkcPR8o2f6yq3O60X-RWmqyoAYN3zXuhg0tN59U_QhI/viewform">Volunteers</a> are very much appreciated. You can also simply send us a <a className="inline" target="_blank" href="https://docs.google.com/forms/d/1rkcPR8o2f6yq3O60X-RWmqyoAYN3zXuhg0tN59U_QhI/viewform">message</a>.
+            </p>
+            <p>
+              We also get a lot of help from friends who wish to remain anonymous. Thanks guys, we love you.
+            </p>
+          </div>
+        </section>
+
       </div>
     );
   }
